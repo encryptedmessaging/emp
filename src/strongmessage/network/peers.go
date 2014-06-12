c74434b14e8b27143230937490825f86ec1095bf
Alex Smith <aosmith@gmail.com>
2014-06-12 12:14:07 -0500

    started moving things to network package

--- src/strongmessage/network/peers.go
+++ src/strongmessage/network/peers.go
@@ -1,13 +1,10 @@
-package objects
+package network
 
 import (
   "fmt"
-  zmq "github.com/alecthomas/gozmq"
   "net"
   "time"
 )
-
-const DEBUG = true
 
 type Peer struct {
   IpAddress net.IP    `json:"ip_address"`
@@ -17,35 +14,4 @@ type Peer struct {
 
 func (p *Peer) TcpString() string {
   return fmt.Sprintf("tcp://%s:%d", p.IpAddress.String(), p.Port)
-}
-
-func (p *Peer) Subscribe(log chan string, messageChannel chan Message, context *zmq.Context) {
-  socket, err := context.NewSocket(zmq.SUB)
-  if err != nil {
-    log <- "Error creating socket"
-    log <- err.Error()
-  } else {
-    log <- fmt.Sprintf("Attempting subscription: %s:%d", p.IpAddress, p.Port)
-    socket.Connect(p.TcpString())
-    for {
-      log <- fmt.Sprintf("Connected: %s:%d", p.IpAddress, p.Port)
-      msg, err := socket.Recv(0)
-      if err != nil {
-        log <- "Socket Error:"
-        log <- err.Error()
-      } else {
-        message, err := MessageFromBytes(log, msg)
-        if err != nil {
-          log <- "Decoding error:"
-          log <- err.Error()
-        } else {
-          if DEBUG == true {
-            log <- "Got message:"
-            log <- fmt.Sprintf("%v", message)
-          }
-          messageChannel <- message
-        }
-      }
-    }
-  }
 }
--- src/strongmessage/objects/message.go
+++ src/strongmessage/objects/message.go
@@ -7,10 +7,10 @@ import (
 )
 
 type Message struct {
-  AddrHash  []byte
-  TxidHash  []byte
-  Timestamp time.Time
-  Content   EncryptedData
+  AddrHash    []byte
+  TxidHash    []byte
+  Timestamp   time.Time
+  Content     EncryptedData
 }
 
 // Lets allow for multiple datatypes even if we don't support them in the first
