92af1731ba3662ca10e4e5850b832a49d19510e1
Alex Smith <aosmith@gmail.com>
2014-07-12 20:13:40 -0500

    fixed start / stop gitignore binary

--- start.sh
+++ start.sh
@@ -1,14 +1,15 @@
-#!/bin/zsh
+#!/bin/bash
 
 # Kill existing process
 if [ -f ./pid ];
 then
   echo 'killing existing process...'
   kill -9 `cat ./pid`
+  rm pid
 fi
 
-
-GOPATH=`pwd`:$GOPATH go run EMP.go >> log &
-echo $! > ./pid
+GOPATH=`pwd`:$GOPATH go build EMP.go
+./EMP >> log &
+echo $! >> ./pid
 sleep 1
 firefox http://localhost:8080/
