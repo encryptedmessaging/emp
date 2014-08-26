bc704a8d03824bf15e4b8439a114a5b0a738079d
Ethan Gordon <ekgordon2@gmail.com>
2014-08-26 15:09:36 -0500

    Switched to BSD License, new Release Candidate

--- src/emp/EMP.go
+++ src/emp/EMP.go
@@ -1,18 +1,12 @@
 /**
+    Copyright 2014 JARST, LLC.
+    
     This file is part of EMP.
-
-    EMP is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
 
     EMP is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the included
+    LICENSE file for more details.
 **/
 
 package main
